ee409592a2aa1676b5969323d92b4c992411baa7
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-28 12:29:06 -0300

    Created the controller CustomersRetrieveController

---
+++ v1/src/main/java/com/api/v1/controllers/CustomersRetrieveController.java
@@ -0,0 +1,36 @@
+package com.api.v1.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.v1.dtos.responses.CustomerResponseDto;
+import com.api.v1.services.customer.CustomersRetrieveService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/v1/customers")
+public class CustomersRetrieveController {
+
+    @Autowired
+    private CustomersRetrieveService service;
+
+    @GetMapping("retrieving")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<CustomerResponseDto> retrieveAll() {
+        return service.retrieveAll();
+    }
+
+    @GetMapping("{ssn}/retrieving")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Mono<CustomerResponseDto> retrieveBySsn(@PathVariable String ssn) {
+        return service.retrieveBySsn(ssn);
+    }
+    
+}
