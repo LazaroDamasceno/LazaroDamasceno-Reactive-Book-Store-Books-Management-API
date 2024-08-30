4cf30188b8e5adcb4ddfe014b21b79b7e9b6e0e8
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-30 02:06:15 -0300

    Created the controller BooksDeletionController

---
+++ v1/src/main/java/com/api/v1/controllers/book/BooksDeletionController.java
@@ -0,0 +1,28 @@
+package com.api.v1.controllers.book;
+
+import com.api.v1.services.book.BooksDeletionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BooksDeletionController {
+
+    @Autowired
+    private BooksDeletionService service;
+
+    @DeleteMapping("obliteration")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteAll() {
+        return service.deleteAll();
+    }
+
+    @DeleteMapping("{isbn}/obliteration")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteByIsbn(@PathVariable String isbn) {
+        return service.deleteByIsbn(isbn);
+    }
+
+}
