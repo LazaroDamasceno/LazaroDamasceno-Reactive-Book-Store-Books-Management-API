924859a6fff3970278455302ce3de47de55926e5
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-30 00:28:56 -0300

    Created the dto BookResponseDto

---
+++ v1/src/main/java/com/api/v1/dtos/responses/BookResponseDto.java
@@ -0,0 +1,12 @@
+package com.api.v1.dtos.responses;
+
+public record BookResponseDto(
+        String fullTitle,
+        String isbn,
+        String author,
+        String field,
+        int numberOfPages,
+        int version,
+        double price
+) {
+}
