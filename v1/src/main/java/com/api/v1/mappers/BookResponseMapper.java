95f661dbf7f0ea8ec35f6878410b1f8312ca345d
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-30 00:28:18 -0300

    Created the mapper BookResponseMapper

---
+++ v1/src/main/java/com/api/v1/mappers/BookResponseMapper.java
@@ -0,0 +1,20 @@
+package com.api.v1.mappers;
+
+import com.api.v1.domain.entities.Book;
+import com.api.v1.dtos.responses.BookResponseDto;
+
+public class BookResponseMapper {
+
+    public static BookResponseDto map(Book book) {
+        return new BookResponseDto(
+                book.getFullTitle(),
+                book.getIsbn(),
+                book.getAuthor(),
+                book.getField(),
+                book.getNumberOfPages(),
+                book.getVersion(),
+                book.getPrice()
+        );
+    }
+
+}
