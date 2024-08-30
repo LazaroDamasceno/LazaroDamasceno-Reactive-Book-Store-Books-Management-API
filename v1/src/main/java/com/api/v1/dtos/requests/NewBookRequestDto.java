6e48f377d3acf32539ba218ba0925ac41e7ff53c
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-30 00:28:45 -0300

    Created the dto NewBookRequestDto

---
+++ v1/src/main/java/com/api/v1/dtos/requests/NewBookRequestDto.java
@@ -0,0 +1,16 @@
+package com.api.v1.dtos.requests;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record NewBookRequestDto(
+        @NotBlank String title,
+        String subtitle,
+        String isbn,
+        @NotBlank String author,
+        @NotBlank String field,
+        int numberOfPages,
+        @Min(1) int version,
+        double price
+) {
+}
