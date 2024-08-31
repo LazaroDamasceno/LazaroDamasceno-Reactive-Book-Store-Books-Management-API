b650a754ed33fc6803da442e14ee2ff2b7dee6c5
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-30 23:27:32 -0300

    Created the interface PurchaseRetrieveService

---
+++ v1/src/main/java/com/api/v1/services/purchase/PurchaseRetrieveService.java
@@ -0,0 +1,17 @@
+package com.api.v1.services.purchase;
+
+import com.api.v1.dtos.responses.PurchaseResponseDto;
+import reactor.core.publisher.Flux;
+
+public interface PurchaseRetrieveService {
+
+    Flux<PurchaseResponseDto> retrieveAll();
+    Flux<PurchaseResponseDto> retrieveByBook(String isbn);
+    Flux<PurchaseResponseDto> retrieveByCustomer(String ssn);
+    Flux<PurchaseResponseDto> retrieveByYear(int year);
+    Flux<PurchaseResponseDto> retrieveByBookAndCustomerAndYear(String isbn,String ssn, int year);
+    Flux<PurchaseResponseDto> retrieveByBookAndCustomer(String isbn,String ssn);
+    Flux<PurchaseResponseDto> retrieveByBookAAndYear(String isbn, int year);
+    Flux<PurchaseResponseDto> retrieveByCustomerAndYear(String ssn, int year);
+
+}
