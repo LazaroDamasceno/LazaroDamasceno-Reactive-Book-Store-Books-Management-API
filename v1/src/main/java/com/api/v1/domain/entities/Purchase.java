e7caffccbb262120e1f9fba73943d87b42b2268b
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-30 22:58:46 -0300

    Updated the constructor Purchase

--- v1/src/main/java/com/api/v1/domain/entities/Purchase.java
+++ v1/src/main/java/com/api/v1/domain/entities/Purchase.java
@@ -1,16 +1,33 @@
 package com.api.v1.domain.entities;
 
+import com.api.v1.utils.StatesSalesTaxRatesUtil;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.ZonedDateTime;
 
 @Document(collection = "v1_purchases")
 public record Purchase(
         @NotNull Book book,
         @NotNull Customer customer,
         double bookPrice,
-        double finalPrice,
         @NotBlank String state,
+        Double saleTax,
+        Double finalPrice,
         String createdAt
 ) {
+
+    public Purchase(Book book, Customer customer, double bookPrice, String state) {
+        this(
+                book,
+                customer,
+                bookPrice,
+                state,
+                StatesSalesTaxRatesUtil.map(state),
+                (1 + StatesSalesTaxRatesUtil.map(state)) * bookPrice,
+                ZonedDateTime.now().toString()
+        );
+    }
+
 }
