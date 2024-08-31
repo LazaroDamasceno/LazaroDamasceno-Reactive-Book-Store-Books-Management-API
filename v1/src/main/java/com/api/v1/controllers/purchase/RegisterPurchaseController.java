01619783bf9b0a394df77ab688c4d5dc783e8148
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-30 23:20:37 -0300

    Created the controller RegisterPurchaseController

---
+++ v1/src/main/java/com/api/v1/controllers/purchase/RegisterPurchaseController.java
@@ -0,0 +1,25 @@
+package com.api.v1.controllers.purchase;
+
+import com.api.v1.domain.entities.Purchase;
+import com.api.v1.dtos.responses.PurchaseResponseDto;
+import com.api.v1.services.purchase.PurchaseRegistrationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/v1/purchases")
+public class RegisterPurchaseController {
+
+    @Autowired
+    private PurchaseRegistrationService service;
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Mono<PurchaseResponseDto> register(@Valid @RequestBody Purchase purchase) {
+        return service.register(purchase);
+    }
+
+}
--- v1/src/main/java/com/api/v1/services/purchase/PurchaseRegistrationServiceImpl.java
+++ v1/src/main/java/com/api/v1/services/purchase/PurchaseRegistrationServiceImpl.java
@@ -4,7 +4,7 @@ import com.api.v1.domain.entities.Purchase;
 import com.api.v1.domain.repositories.PurchaseRepository;
 import com.api.v1.dtos.responses.PurchaseResponseDto;
 import com.api.v1.mappers.PurchaseResponseMapper;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -16,7 +16,7 @@ class PurchaseRegistrationServiceImpl implements PurchaseRegistrationService {
     private PurchaseRepository repository;
 
     @Override
-    public Mono<PurchaseResponseDto> register(@NotNull Purchase purchase) {
+    public Mono<PurchaseResponseDto> register(@Valid Purchase purchase) {
         return repository
                 .save(purchase)
                 .flatMap(savedPurchase -> Mono.just(PurchaseResponseMapper.map(savedPurchase)));
