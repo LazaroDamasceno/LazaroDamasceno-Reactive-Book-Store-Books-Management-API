af186d43a603979ad6767606bc4e2f921f16a50e
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-30 22:06:25 -0300

    Updated the dto PurchaseResponseDto

--- v1/src/main/java/com/api/v1/dtos/responses/PurchaseResponseDto.java
+++ v1/src/main/java/com/api/v1/dtos/responses/PurchaseResponseDto.java
@@ -4,6 +4,7 @@ public record PurchaseResponseDto(
         BookResponseDto book,
         CustomerResponseDto customer,
         double bookPrice,
+        double saleTax,
         double finalPrice,
         String state,
         String createdAt
