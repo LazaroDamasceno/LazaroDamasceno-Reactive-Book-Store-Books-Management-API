7f7ba947e44d776f261033fec19d4ae5f87a69f7
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-01 01:56:39 -0300

    Corrected the message of CustomerDataDeletionException

--- v1/src/main/java/com/api/v1/exceptions/customer/CustomerDataDeletionException.java
+++ v1/src/main/java/com/api/v1/exceptions/customer/CustomerDataDeletionException.java
@@ -3,7 +3,7 @@ package com.api.v1.exceptions.customer;
 public class CustomerDataDeletionException extends RuntimeException {
 
     public CustomerDataDeletionException() {
-        super("There entity named 'Customer' registered.");
+        super("There no entity 'Customer' registered.");
     }
 
 }
