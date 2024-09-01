923be9834498ffc7af2466257b76b7e6cc8dd056
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-01 00:09:53 -0300

    Unified CustomerRegistrationController, CustomerDeletionController, CustomerRetrieveController and CustomerUpdateController into a single controller named CustomerController

---
+++ v1/src/main/java/com/api/v1/controllers/customer/CustomerController.java
@@ -0,0 +1,69 @@
+package com.api.v1.controllers.customer;
+
+import com.api.v1.dtos.requests.NewCustomerRequestDto;
+import com.api.v1.dtos.requests.PaginationRequestDto;
+import com.api.v1.dtos.responses.CustomerResponseDto;
+import com.api.v1.services.customer.CustomerRegistrationService;
+import com.api.v1.services.customer.CustomerUpdateService;
+import com.api.v1.services.customer.CustomersDeletionService;
+import com.api.v1.services.customer.CustomersRetrieveService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/v1/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRegistrationService registrationService;
+
+    @Autowired
+    private CustomersDeletionService deletionService;
+
+    @Autowired
+    private CustomersRetrieveService retrieveService;
+
+    @Autowired
+    private CustomerUpdateService updateService;
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Mono<CustomerResponseDto> register(@Valid @RequestBody NewCustomerRequestDto request) {
+        return registrationService.register(request);
+    }
+
+    @DeleteMapping
+    public Mono<Void> deleteAll() {
+        return deletionService.deleteAll();
+    }
+
+    @DeleteMapping("{ssn}")
+    public Mono<Void> deleteBySsn(@PathVariable String ssn) {
+        return deletionService.deleteBySsn(ssn);
+    }
+
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<CustomerResponseDto> retrieveAll(@Valid @RequestBody PaginationRequestDto pagination) {
+        return retrieveService.retrieveAll(pagination);
+    }
+
+    @GetMapping("{ssn}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Mono<CustomerResponseDto> retrieveBySsn(@PathVariable String ssn) {
+        return retrieveService.retrieveBySsn(ssn);
+    }
+
+    @PutMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public Mono<CustomerResponseDto> update(
+            @Valid @RequestBody NewCustomerRequestDto request
+    ) {
+        return updateService.update(request);
+    }
+    
+}
