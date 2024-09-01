40c43c75ab621790b35d126f9548bbd748857e1e
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-01 02:51:21 -0300

    Refactored PurchaseDeletionServiceImpl

--- v1/src/main/java/com/api/v1/services/purchase/PurchaseDeletionService.java
+++ v1/src/main/java/com/api/v1/services/purchase/PurchaseDeletionService.java
@@ -1,18 +1,18 @@
 package com.api.v1.services.purchase;
 
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.Mono;
 
 public interface PurchaseDeletionService {
 
     Mono<Void> deleteById(String id);
     Mono<Void> deleteAll();
-    Flux<Void> deleteByCustomer(String ssn);
-    Flux<Void> deleteByBook(String isbn);
-    Flux<Void> deleteByYear(int year);
-    Flux<Void> deleteByCustomerAndBookAndYear(String ssn, String isbn, int year);
-    Flux<Void> deleteByCustomerAndBook(String ssn, String isbn);
-    Flux<Void> deleteByCustomerAndYear(String ssn, int year);
-    Flux<Void> deleteByBookAndYear(String isbn, int year);
+    Mono<Void> deleteByCustomer(String ssn);
+    Mono<Void> deleteByBook(String isbn);
+    Mono<Void> deleteByYear(int year);
+    Mono<Void> deleteByCustomerAndBookAndYear(String ssn, String isbn, int year);
+    Mono<Void> deleteByCustomerAndBook(String ssn, String isbn);
+    Mono<Void> deleteByCustomerAndYear(String ssn, int year);
+    Mono<Void> deleteByBookAndYear(String isbn, int year);
 
 }
--- v1/src/main/java/com/api/v1/services/purchase/PurchaseDeletionServiceImpl.java
+++ v1/src/main/java/com/api/v1/services/purchase/PurchaseDeletionServiceImpl.java
@@ -3,12 +3,13 @@ package com.api.v1.services.purchase;
 import com.api.v1.domain.entities.Book;
 import com.api.v1.domain.entities.Customer;
 import com.api.v1.domain.repositories.PurchaseRepository;
+import com.api.v1.exceptions.purchase.PurchaseDataDeletionException;
 import com.api.v1.exceptions.purchase.PurchaseNotFoundException;
 import com.api.v1.utils.book.BookFinderUtil;
 import com.api.v1.utils.customer.CustomerFinderUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.Mono;
 
 import java.time.ZonedDateTime;
@@ -36,94 +37,157 @@ class PurchaseDeletionServiceImpl implements PurchaseDeletionService {
 
     @Override
     public Mono<Void> deleteAll() {
-        return repository.deleteAll();
-    }
-
-    @Override
-    public Flux<Void> deleteByCustomer(String ssn) {
-        return customerFinderUtil
-                .find(ssn)
-                .flatMapMany(customer -> repository
-                            .findAll()
-                            .filter(e -> e.customer().equals(customer))
-                )
-                .flatMap(purchase -> repository.delete(purchase));
-    }
-
-    @Override
-    public Flux<Void> deleteByBook(String isbn) {
-        return bookFinderUtil
-                .find(isbn)
-                .flatMapMany(book -> repository
-                        .findAll()
-                        .filter(e -> e.book().equals(book))
-                )
-                .flatMap(purchase -> repository.delete(purchase));
-    }
-
-    @Override
-    public Flux<Void> deleteByYear(int year) {
         return repository
                 .findAll()
-                .filter(e -> ZonedDateTime.parse(e.createdAt()).getYear() == year)
-                .flatMap(purchase -> repository.delete(purchase));
+                .hasElements()
+                .flatMap(exists -> {
+                    if (exists) return repository.deleteAll();
+                    return Mono.error(PurchaseDataDeletionException::new);
+                });
     }
 
     @Override
-    public Flux<Void> deleteByCustomerAndBookAndYear(String ssn, String isbn, int year) {
-        Mono<Customer> customerMono = customerFinderUtil.find(ssn);
-        Mono<Book> bookMono = bookFinderUtil.find(isbn);
-        return customerMono
-                .zipWith(bookMono)
-                .flatMapMany(tuple -> repository
-                            .findAll()
-                            .filter(e -> e.book().equals(tuple.getT2())
-                                    && e.customer().equals(tuple.getT1())
-                                    && ZonedDateTime.now().getYear() == year
+    public Mono<Void> deleteByCustomer(String ssn) {
+        return repository
+                .findAll()
+                .hasElements()
+                .flatMap(exists -> {
+                    if (!exists) return Mono.error(PurchaseDataDeletionException::new);
+                    return customerFinderUtil
+                            .find(ssn)
+                            .flatMapMany(customer -> repository
+                                    .findAll()
+                                    .filter(e -> e.customer().equals(customer))
                             )
                             .flatMap(purchase -> repository.delete(purchase))
-                );
+                            .then();
+                });
     }
 
     @Override
-    public Flux<Void> deleteByCustomerAndBook(String ssn, String isbn) {
-        Mono<Customer> customerMono = customerFinderUtil.find(ssn);
-        Mono<Book> bookMono = bookFinderUtil.find(isbn);
-        return customerMono
-                .zipWith(bookMono)
-                .flatMapMany(tuple -> repository
-                        .findAll()
-                        .filter(e -> e.book().equals(tuple.getT2())
-                                && e.customer().equals(tuple.getT1())
-                        )
-                        .flatMap(purchase -> repository.delete(purchase))
-                );
+    public Mono<Void> deleteByBook(String isbn) {
+        return repository
+                .findAll()
+                .hasElements()
+                .flatMap(exists -> {
+                    if (!exists) return Mono.error(PurchaseDataDeletionException::new);
+                    return bookFinderUtil
+                            .find(isbn)
+                            .flatMapMany(book -> repository
+                                    .findAll()
+                                    .filter(e -> e.book().equals(book))
+                            )
+                            .flatMap(purchase -> repository.delete(purchase))
+                            .then();
+                });
+
     }
 
     @Override
-    public Flux<Void> deleteByCustomerAndYear(String ssn, int year) {
-        return customerFinderUtil
-                .find(ssn)
-                .flatMapMany(customer -> repository
-                        .findAll()
-                        .filter(e -> e.customer().equals(customer)
-                                && ZonedDateTime.now().getYear() == year
-                        )
-                        .flatMap(purchase -> repository.delete(purchase))
-                );
+    public Mono<Void> deleteByYear(int year) {
+        return repository
+                .findAll()
+                .hasElements()
+                .flatMap(exists -> {
+                    if (!exists) return Mono.error(PurchaseDataDeletionException::new);
+                    return repository
+                            .findAll()
+                            .filter(e -> ZonedDateTime.parse(e.createdAt()).getYear() == year)
+                            .flatMap(purchase -> repository.delete(purchase))
+                            .then();
+                });
     }
 
     @Override
-    public Flux<Void> deleteByBookAndYear(String isbn, int year) {
-        return bookFinderUtil
-                .find(isbn)
-                .flatMapMany(book -> repository
-                        .findAll()
-                        .filter(e -> e.book().equals(book)
-                                && ZonedDateTime.now().getYear() == year
-                        )
-                        .flatMap(purchase -> repository.delete(purchase))
-                );
+    public Mono<Void> deleteByCustomerAndBookAndYear(String ssn, String isbn, int year) {
+        return repository
+                .findAll()
+                .hasElements()
+                .flatMap(exists -> {
+                    if (!exists) return Mono.error(PurchaseDataDeletionException::new);
+                    return Mono.defer(() -> {
+                        Mono<Customer> customerMono = customerFinderUtil.find(ssn);
+                        Mono<Book> bookMono = bookFinderUtil.find(isbn);
+                        return customerMono
+                                .zipWith(bookMono)
+                                .flatMapMany(tuple -> repository
+                                        .findAll()
+                                        .filter(e -> e.book().equals(tuple.getT2())
+                                                && e.customer().equals(tuple.getT1())
+                                                && ZonedDateTime.now().getYear() == year
+                                        )
+                                        .flatMap(purchase -> repository.delete(purchase))
+                                )
+                                .then();
+                    });
+                });
+    }
+
+    @Override
+    public Mono<Void> deleteByCustomerAndBook(String ssn, String isbn) {
+        return repository
+                .findAll()
+                .hasElements()
+                .flatMap(exists -> {
+                    if (!exists) return Mono.error(PurchaseDataDeletionException::new);
+                    return Mono.defer(() -> {
+                        Mono<Customer> customerMono = customerFinderUtil.find(ssn);
+                        Mono<Book> bookMono = bookFinderUtil.find(isbn);
+                        return customerMono
+                                .zipWith(bookMono)
+                                .flatMapMany(tuple -> repository
+                                        .findAll()
+                                        .filter(e -> e.book().equals(tuple.getT2())
+                                                && e.customer().equals(tuple.getT1())
+                                        )
+                                        .flatMap(purchase -> repository.delete(purchase))
+                                )
+                                .then();
+                    });
+                });
+
+    }
+
+    @Override
+    public Mono<Void> deleteByCustomerAndYear(String ssn, int year) {
+        return repository
+                .findAll()
+                .hasElements()
+                .flatMap(exists -> {
+                    if (!exists) return Mono.error(PurchaseDataDeletionException::new);
+                    return customerFinderUtil
+                                .find(ssn)
+                                .flatMapMany(customer -> repository
+                                        .findAll()
+                                        .filter(e -> e.customer().equals(customer)
+                                                && ZonedDateTime.now().getYear() == year
+                                        )
+                                        .flatMap(purchase -> repository.delete(purchase))
+                                )
+                                .then();
+                });
+
+    }
+
+    @Override
+    public Mono<Void> deleteByBookAndYear(String isbn, int year) {
+        return repository
+                .findAll()
+                .hasElements()
+                .flatMap(exists -> {
+                    if (!exists) return Mono.error(PurchaseDataDeletionException::new);
+                    return bookFinderUtil
+                            .find(isbn)
+                            .flatMapMany(book -> repository
+                                    .findAll()
+                                    .filter(e -> e.book().equals(book)
+                                            && ZonedDateTime.now().getYear() == year
+                                    )
+                                    .flatMap(purchase -> repository.delete(purchase))
+                            )
+                            .then();
+                });
+
     }
 
 }
