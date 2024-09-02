cf220871e8c9e76b1df110cc7e9dbd4f4286c9c4
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-02 01:27:23 -0300

    Replaced UUID with ObjectId in the repositories

--- v1/src/main/java/com/api/v1/domain/repositories/PurchaseRepository.java
+++ v1/src/main/java/com/api/v1/domain/repositories/PurchaseRepository.java
@@ -1,13 +1,12 @@
 package com.api.v1.domain.repositories;
 
 import com.api.v1.domain.entities.Purchase;
+import org.bson.types.ObjectId;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
-import java.util.UUID;
-
-public interface PurchaseRepository extends ReactiveCrudRepository<Purchase, UUID> {
+public interface PurchaseRepository extends ReactiveCrudRepository<Purchase, ObjectId> {
 
     Flux<Purchase> findBy(Pageable pageable);
 
