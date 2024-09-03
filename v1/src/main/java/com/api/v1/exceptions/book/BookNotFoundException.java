19f10bf055ae8a0d639e8eda7bc112d78ad15432
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-03 01:08:33 -0300

    Fixed BookNotFoundException

--- v1/src/main/java/com/api/v1/exceptions/book/BookNotFoundException.java
+++ v1/src/main/java/com/api/v1/exceptions/book/BookNotFoundException.java
@@ -3,7 +3,7 @@ package com.api.v1.exceptions.book;
 public class BookNotFoundException extends RuntimeException {
 
     public BookNotFoundException(String isbn) {
-        super("The ISBN %s was not found.");
+        super("The ISBN %s was not found.".formatted(isbn));
     }
 
 }
