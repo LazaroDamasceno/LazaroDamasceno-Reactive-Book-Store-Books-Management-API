af00b08114dfd1c2b71044cead9c52fad6ab4c3c
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2024-09-04 00:14:19 -0300

    Update CustomerController.java

--- v1/src/main/java/com/api/v1/controllers/customer/CustomerController.java
+++ v1/src/main/java/com/api/v1/controllers/customer/CustomerController.java
@@ -41,12 +41,6 @@ public class CustomerController {
         return deletionService.deleteAll();
     }
 
-    @DeleteMapping("{ssn}")
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public Mono<Void> deleteBySsn(@PathVariable String ssn) {
-        return deletionService.deleteBySsn(ssn);
-    }
-
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
     public Flux<CustomerResponseDto> retrieveAll() {
