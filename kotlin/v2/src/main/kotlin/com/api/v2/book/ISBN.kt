73bf47e4eb6fd52a968456c61b8c013e715748be
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-26 20:24:31 -0300

    Created the annotation ISBN

---
+++ kotlin/v2/src/main/kotlin/com/api/v2/book/ISBN.kt
@@ -0,0 +1,17 @@
+package com.api.v2.book
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+import kotlin.annotation.AnnotationRetention
+import kotlin.annotation.AnnotationTarget
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(
+    AnnotationTarget.FIELD,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.TYPE
+)
+@NotBlank
+@Size(min=13, max=13)
+annotation class ISBN(val regex: String = "\\d{13}")
