174ce5b30bfff9fd221b84f59284e9b49d49480d
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-26 20:14:17 -0300

    Added the method modify and fullTitle to Book. Also, a constructor was set

--- kotlin/v2/src/main/kotlin/com/api/v2/book/Book.kt
+++ kotlin/v2/src/main/kotlin/com/api/v2/book/Book.kt
@@ -1,11 +1,13 @@
 package com.api.v2.book
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
 import java.time.Instant
 import java.time.ZoneId
 import java.util.UUID
 
+@Document(collection = "v2_books")
 data class Book(
     @Id
     var id: UUID,
@@ -30,8 +32,60 @@ data class Book(
     @Field
     val creationZoneId: ZoneId,
     @Field
-    var modifiedAt: Instant,
+    var modifiedAt: Instant?,
     @Field
-    var modificationZoneId: ZoneId
+    var modificationZoneId: ZoneId?
 ) {
+
+    constructor(
+        title: String,
+        subtitle: String?,
+        isbn: String,
+        author: String,
+        field: String,
+        publisher: String,
+        numberOfPages: Int,
+        version: Int
+    ) : this(
+        UUID.randomUUID(),
+        title,
+        subtitle,
+        isbn,
+        author,
+        field,
+        publisher,
+        numberOfPages,
+        version,
+        Instant.now(),
+        ZoneId.systemDefault(),
+        null,
+        null
+    )
+
+    fun modify(
+        title: String,
+        subtitle: String?,
+        author: String,
+        field: String,
+        publisher: String,
+        numberOfPages: Int,
+        version: Int
+    ): Book {
+        this.title = title
+        this.subtitle = subtitle
+        this.author = author
+        this.field = field
+        this.publisher = publisher
+        this.numberOfPages = numberOfPages
+        this.version = version
+        return this
+    }
+
+    fun fullTitle(): String {
+        if (subtitle.isNullOrEmpty()) {
+            return title
+        }
+        return "$title: $subtitle"
+    }
+
 }
